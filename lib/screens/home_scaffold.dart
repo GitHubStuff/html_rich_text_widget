d86bbf787eb55afb33f674d6668f66cc9fa79c48
Steven <github@icodeforyou.com>
2023-11-01 17:40:14 -0400

    Initial commit

---
+++ lib/screens/home_scaffold.dart
@@ -0,0 +1,35 @@
+// ignore_for_file: sort_child_properties_last
+
+import 'package:flutter/material.dart';
+
+import '../gen/assets.gen.dart';
+
+class HomeScaffold extends StatelessWidget {
+  const HomeScaffold({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: homeWidget(context),
+      floatingActionButton: null,
+    );
+  }
+
+  Widget homeWidget(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            width: 200,
+            height: 200,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Assets.images.ltmm1024x1024.image(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
